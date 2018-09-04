1457b9e62780cd5d5bf6ef5bb717b6f1725c5699
alex jenter <alex@attribute.ch>
2018-09-05 01:42:06 +0200

    ADD overview first draft

--- src/App.js
+++ src/App.js
@@ -1,6 +1,33 @@
 import React, { Component } from "react";
+import data from "./data"
 import logo from "./logo.svg";
 import "./App.css";
+
+
+
+const TextTile = ({title, type, body}) => (
+  <div className="tile tile--text">
+    <h2 className="tile__title">{title}</h2>
+    <p className="tile__body" dangerouslySetInnerHTML={{ __html: body }}/>
+  </div>
+)
+
+const ImageTile = ({ title, src }) => (
+  <div className="tile tile--image">
+    <h2 className="tile__title">{title}</h2>
+    <img className="tile__image" src={src} alt=""/>
+  </div>
+)
+
+const Tile = (props) => {
+  const {title, type, body} = props
+  return {
+    markup: <TextTile {...props} />,
+    image: <ImageTile {...props} />
+  }[type] || <p>type not defined</p>
+}
+
+  
 
 class App extends Component {
   render() {
@@ -8,17 +35,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Burkhard & Lüthi Architektur GmbH</h1>
-          <p>
-            Dufourstrasse 35
-            <br/>
-            CH 8008 Zürich
-            <br/>
-            <a href="tel:+41 44 262 50 40">+41 44 262 50 40</a>
-            <br/>
-            <a href="mailto:bul@burkhard-luethi.ch">bul@burkhard-luethi.ch</a>
-            <br/>
-          </p>
         </header>
+        <main>
+          { data.map(x => <Tile {...x}/>)}
+        </main>
       </div>
     );
   }
---
+++ src/data.js
@@ -0,0 +1,51 @@
+import img0 from "./images/img0.jpg"
+import img1 from "./images/img1.jpg"
+import img2 from "./images/img2.jpg"
+import img3 from "./images/img3.jpg"
+import img4 from "./images/img4.jpg"
+import img5 from "./images/img5.jpg"
+
+export default [
+  {
+    key: 9794792,
+    title: "contact",
+    type: "markup",
+    body: "<p> Dufourstrasse 35 <br/> CH 8008 Zürich <br/> <a href='tel:+41 44 262 50 40'>+41 44 262 50 40</a> <br/> <a href='mailto:bul@burkhard-luethi.ch'>bul@burkhard-luethi.ch</a> <br/> </p>"
+  },
+  {
+    key: 9794700,
+    title: "projekt 939",
+    type: "image",
+    src: img0
+  },
+  {
+    key: 9794701,
+    title: "projekt 939",
+    type: "image",
+    src: img1
+  },
+  {
+    key: 9794702,
+    title: "projekt 939",
+    type: "image",
+    src: img2
+  },
+  {
+    key: 9794703,
+    title: "projekt 939",
+    type: "image",
+    src: img3
+  },
+  {
+    key: 9794704,
+    title: "projekt 939",
+    type: "image",
+    src: img4
+  },
+  {
+    key: 9794705,
+    title: "projekt 939",
+    type: "image",
+    src: img5
+  }
+]
