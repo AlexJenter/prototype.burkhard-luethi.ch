35cff9d61b2021f9f8c7539cc99ab4f27e350248
jenteralex <alexjenter@alexs-MacBook-Pro.local>
2018-11-09 19:05:21 +0100

    Page dots

--- src/components/PageDots/index.js
+++ src/components/PageDots/index.js
@@ -1,22 +1,6 @@
 import React from "react";
 import "./index.css";
 
-export default ({ position = 0, count = 10, setPosition }) => {
-  const dots = Array.from(new Array(count));
-  dots.fill(false);
-  dots[position] = "true";
-
-  return (
-    <div className="page-dots">
-      <ul>
-        {dots &&
-          dots.map((bool, i) => (
-            <button
-              onClick={setPosition(i)}
-              className={`page-dot${bool ? " page-dot--active" : ""}`}
-            ></button>
-          ))}
-      </ul>
-    </div>
-  );
-};
+export default ({ position = 0, count, setPosition }) => (
+  <div className="page-dots">{`${position}|${count}`}</div>
+);
